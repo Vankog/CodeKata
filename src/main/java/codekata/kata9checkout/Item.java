48899a3035d9512369ea75b21a86e3ad97e287e8
Vankog <m.vankog@gmail.com>
2019-04-12 13:25:20 +0200

    add retrieval of item price

--- src/main/java/codekata/kata9checkout/Item.java
+++ src/main/java/codekata/kata9checkout/Item.java
@@ -2,8 +2,13 @@ package codekata.kata9checkout;
 
 public class Item {
 
+    private int price;
 
     public Item(int price) {
+        this.price = price;
+    }
 
+    public int getPrice() {
+        return price;
     }
 }
